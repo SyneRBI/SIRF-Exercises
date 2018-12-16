0cec3ffeb77a0e01962dd7ccf93e95fd0f4f5b9d
Ben Thomas <ben.thomas1984@gmail.com>
2018-12-16 00:46:52 +0000

    Use NEMA IQ data from Zenodo

--- scripts/download_PET_data.sh
+++ scripts/download_PET_data.sh
@@ -38,25 +38,25 @@ function download {
 set -e
 trap "echo some error occured. Retry" ERR
 
-URL=http://ccppetmr.azureedge.net/psmr-data/
+URL=https://zenodo.org/record/1304454/files/
 destination=${1:-~/data}
 
 mkdir -p ${destination}
 cd ${destination}
 
-filename=NEMA.tar.gz
+filename=NEMA_IQ.zip
 # (re)download md5 checksum
 rm -f ${filename}.md5
 # hard-wired md5 for now
 #wget ${URL}${filename}.md5
-echo "9e8efeeaa57bc613b4d8b5c4af091534 ${filename}" > ${filename}.md5
+echo "ef848b8f6d5fd57b072a953b374ba4da ${filename}" > ${filename}.md5
 
 download $filename
 
 # specific to PET data
 
 echo "Unpacking $filename"
-tar xzf ${filename}
+unzip ${filename}
 
 # make symbolic links in the normal demo directory
 if test -z "$SIRF_PATH"
@@ -69,7 +69,7 @@ final_dest=$SIRF_PATH/data/examples/PET/mMR
 echo "Creating symbolic links in ${final_dest} "
 cd ${final_dest}
 rm -f 20170809_NEMA_60min_UCL.l*
-ln -s ${destination}/NEMA/20170809_NEMA_60min_UCL.l.hdr
-ln -s ${destination}/NEMA/20170809_NEMA_60min_UCL.l
+ln -s ${destination}/NEMA_IQ/20170809_NEMA_60min_UCL.l.hdr
+ln -s ${destination}/NEMA_IQ/20170809_NEMA_60min_UCL.l
 
 echo "All done!"
