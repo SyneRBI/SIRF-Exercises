62c8c49adc11884918e29881161b67b5472f0944
Georg Schramm <georg.schramm@kuleuven.be>
2024-05-03 18:04:32 +0200

    add first figure

--- notebooks/Deep_Learning_listmode_PET/00_introduction.py
+++ notebooks/Deep_Learning_listmode_PET/00_introduction.py
@@ -5,4 +5,7 @@
 # 1. What is listmode PET reconstruction and why is it attractive for combining DL and reconstruction.
 # 2. Basic building blocks / architectues of unrolled reconstruction networks.
 # 3. Essential blocks of training a neural network in pytorch (model setup, data loading, gradient backpropagation)
-#    and what we are missing from pytorch to build an unrolled PET reconstruction network. 
+#    and what we are missing from pytorch to build an unrolled PET reconstruction network.
+
+# %% [markdown]
+# ![title](figs/varnet.drawio.svg)
