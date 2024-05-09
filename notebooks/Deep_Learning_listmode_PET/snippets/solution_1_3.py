e9b481d4b6e83f588a519132261fd41692964016
Georg Schramm <georg.schramm@kuleuven.be>
2024-05-09 18:35:13 +0200

    use inverse sens images in updates

--- notebooks/Deep_Learning_listmode_PET/snippets/solution_1_3.py
+++ notebooks/Deep_Learning_listmode_PET/snippets/solution_1_3.py
@@ -1,20 +1,24 @@
-# setup an image to store the step size
-step = acq_data.create_uniform_image(value=1, xy=nxny)
+# calculate the inverse of the subset sensitivity images, correctly accounting for voxels
+# where the sensitivity images are zero
+
+inverse_sens_images = []
+
+for i in range(num_subsets):
+    inverse_sens_image = acq_data.create_uniform_image(value=0, xy=nxny)
+    inverse_sens_image_np = np.zeros(
+        inverse_sens_image.shape, dtype=inverse_sens_image.as_array().dtype
+    )
+    sens_image_np = obj_fun.get_subset_sensitivity(i).as_array()
+    np.divide(1, sens_image_np, out=inverse_sens_image_np, where=sens_image_np > 0)
+    inverse_sens_image.fill(inverse_sens_image_np)
+    inverse_sens_images.append(inverse_sens_image)
 
 for it in range(num_iter):
     for i in range(num_subsets):
         subset_grad = obj_fun.gradient(recon, i)
-        tmp = np.zeros(recon.shape, dtype=recon.as_array().dtype)
         # use np.divide for the element-wise division for all elements where
         # the sensitivity image is greater than 0
-        np.divide(
-            recon.as_array(),
-            obj_fun.get_subset_sensitivity(i).as_array(),
-            out=tmp,
-            where=obj_fun.get_subset_sensitivity(i).as_array() > 0,
-        )
-        step.fill(tmp)
-        recon = recon + step * subset_grad
+        recon = recon + recon * inverse_sens_images[i] * subset_grad
 
 fig2, ax2 = plt.subplots(1, 1, figsize=(4, 4), tight_layout=True)
 ax2.imshow(recon.as_array()[71, :, :], cmap="Greys", vmin=0, vmax=vmax)
--- notebooks/Deep_Learning_listmode_PET/snippets/solution_1_4.py
+++ notebooks/Deep_Learning_listmode_PET/snippets/solution_1_4.py
@@ -5,25 +5,30 @@ lm_obj_fun.set_up(initial_image)
 lm_recon = initial_image.copy()
 lm_recon.fill(obj_fun.get_subset_sensitivity(0).as_array() > 0)
 
-# setup an image to store the step size
-step = acq_data.create_uniform_image(value=1, xy=nxny)
+lm_inverse_sens_images = []
+
+for i in range(num_subsets):
+    lm_inverse_sens_image = acq_data.create_uniform_image(value=0, xy=nxny)
+    lm_inverse_sens_image_np = np.zeros(
+        inverse_sens_image.shape, dtype=inverse_sens_image.as_array().dtype
+    )
+    # lm_sens_image_np = lm_obj_fun.get_subset_sensitivity(i).as_array()
+    lm_sens_image_np = obj_fun.get_subset_sensitivity(
+        i
+    ).as_array()  # LM obj_func.get_subset_sensitivity not working so far ...
+    np.divide(
+        1, lm_sens_image_np, out=lm_inverse_sens_image, where=lm_sens_image_np > 0
+    )
+    lm_inverse_sens_image.fill(inverse_sens_image_np)
+    lm_inverse_sens_images.append(inverse_sens_image)
 
 for it in range(num_iter):
     for i in range(num_subsets):
         subset_grad = lm_obj_fun.gradient(recon, i)
-        tmp = np.zeros(recon.shape, dtype=recon.as_array().dtype)
         # use np.divide for the element-wise division for all elements where
         # the sensitivity image is greater than 0
-        np.divide(
-            recon.as_array(),
-            obj_fun.get_subset_sensitivity(
-                i
-            ).as_array(),  ### HACK: lm_obj_fun.get_subset_sensitivity(i) still returns None
-            out=tmp,
-            where=obj_fun.get_subset_sensitivity(i).as_array() > 0,
-        )
-        step.fill(tmp)
-        lm_recon = lm_recon + step * subset_grad
+        recon = recon + recon * lm_inverse_sens_images[i] * subset_grad
+
 
 fig4, ax4 = plt.subplots(1, 1, figsize=(4, 4), tight_layout=True)
 ax4.imshow(lm_recon.as_array()[71, :, :], cmap="Greys", vmin=0, vmax=vmax)
