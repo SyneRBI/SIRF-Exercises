ac04f5725c9c3452ec6cbe8d398b11ecbb2a7fad
NicoleJurjew <nicole.jurjew.20@ucl.ac.uk>
2024-05-19 15:53:13 +0100

    notebooks/PET/Solution_Snippets/DIY_OSEM_01_MLEM.py: bugfix in MLEM function

--- notebooks/PET/Solution_Snippets/DIY_OSEM_01_MLEM.py
+++ notebooks/PET/Solution_Snippets/DIY_OSEM_01_MLEM.py
@@ -6,4 +6,6 @@ def MLEM(acquired_data, acq_model, initial_image, num_iterations):
         quotient.fill(numpy.nan_to_num(quotient.as_array()))
         mult_update = acq_model.backward(quotient)/sensitivity         # A^t * quotient / A^t1
         mult_update.fill(numpy.nan_to_num(mult_update.as_array()))
+        estimated_image *= mult_update
+        estimated_image.maximum(0)
     return estimated_image
